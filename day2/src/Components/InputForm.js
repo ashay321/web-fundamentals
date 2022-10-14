a56dbe9822f99df40c36737e72866b0d1467f42a
ashay321 <ashay.wanjarkar1@getfareye.com>
2022-10-14 16:43:38 +0530

    changed input form

--- day2/src/Components/InputForm.js
+++ day2/src/Components/InputForm.js
@@ -28,37 +28,26 @@ export default class InputForm extends PureComponent {
             <div className="input-form-block">
                 <div className='input-form'>
                     <Form>
-                        <Form.Item
-                            label="Username"
-                            name="username"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
-                            <Input type="text" placeholder='Name' value={this.state.username} onChange={(e)=>{this.setState({username: e.target.value})}}/>
-                        </Form.Item>
+                        
+                        <div className='input-field'>
+                            <div className='input-field-label'>Name:</div>
+                            <Input required type="text" placeholder='Name' value={this.state.username} onChange={(e)=>{this.setState({username: e.target.value})}}/>
+                        </div>
 
-                        <Form.Item
-                            label="Standard"
-                            name="standard"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
+                        <div className='input-field'>
+                            <div className='input-field-label'>Degree:</div>
                             <Input type="text" placeholder='Degree' value={this.state.standard} onChange={(e)=>{this.setState({standard: e.target.value})}}/>
-                        </Form.Item>
+                        </div>
 
-                        <Form.Item
-                            label="Mobile number"
-                            name="mobileNumber"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
+                        <div className='input-field'>
+                            <div className='input-field-label'>Degree:</div>
                             <Input type="text" placeholder='Mobile' value={this.state.mobileNumber} onChange={(e)=>{this.setState({mobileNumber: e.target.value})}}/>
-                        </Form.Item>
+                        </div>
 
-                        <Form.Item
-                            label="Blood grp"
-                            name="bloodGrp"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
+                        <div className='input-field'>
+                            <div className='input-field-label'>Degree:</div>
                             <Input type="text" placeholder='Blood group' value={this.state.bloodGrp} onChange={(e)=>{this.setState({bloodGrp: e.target.value})}}/>
-                        </Form.Item>
+                        </div>
 
                         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                             <Button type="primary" onClick={this.handleSubmit}>
